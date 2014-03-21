df6d66b63ec660830724c23acaf8571e7ff895d8
Barosl LEE <vcs@barosl.com>
2014-03-21 23:11:44 +0900

    gulpfile.js: Use gulp-newer instead of gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,39 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
-var watch = require('gulp-watch');
+var newer = require('gulp-newer');
 
 var coffee = require('gulp-coffee');
 var less = require('gulp-less');
 
+var srcs = {
+    js: 'js/*.coffee',
+    css: 'css/*.less'
+};
+
+var dests = {
+    js: 'build/js',
+    css: 'build/css'
+};
+
 gulp.task('js', function() {
-    return gulp.src('js/*.coffee')
-        .pipe(watch())
+    return gulp.src(srcs.js)
+        .pipe(newer(dests.js))
         .pipe(plumber())
         .pipe(coffee())
-        .pipe(gulp.dest('build/js'));
+        .pipe(gulp.dest(dests.js));
 });
 
 gulp.task('css', function() {
-    return gulp.src('css/*.less')
-        .pipe(watch())
+    return gulp.src(srcs.css)
+        .pipe(newer(dests.css))
         .pipe(plumber())
         .pipe(less())
-        .pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest(dests.css));
 });
 
-gulp.task('default', ['js', 'css']);
+gulp.task('watch', function() {
+    gulp.watch(srcs.js, ['js']);
+    gulp.watch(srcs.css, ['css']);
+});
+
+gulp.task('default', ['js', 'css', 'watch']);
